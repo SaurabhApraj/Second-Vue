58c33a633a250fdaa6e8ac2e8eadaddb7d6b9693
SaurabhApraj <saurabhapraj7@gmail.com>
2020-07-18 23:30:10 +0530

    DataBinding

---
+++ src/components/DataBind.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <!-- <h1>{{message}}</h1> -->
+    <!-- <input type="text" v-model="message" placeholder="Message" /> -->
+    <p style="white-space:pre-line">{{message}}</p>
+    <textarea type="text" v-model="message"></textarea>
+  </div>
+</template>
+<script>
+export default {
+  name: "DataBind",
+  data() {
+    return {
+      message: ""
+    };
+  }
+};
+</script>
