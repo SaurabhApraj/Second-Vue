409116a93289f986431cea1104a54b828ca62abf
SaurabhApraj <saurabhapraj7@gmail.com>
2020-07-18 23:55:43 +0530

    Props

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,31 @@
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-    <DataBind />
+    <!-- <DataBind /> -->
+    <Child v-bind:users="users" />
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import DataBind from "./components/DataBind";
+// import DataBind from "./components/DataBind";
+import Child from "./components/Child";
 export default {
   name: "App",
   components: {
     // HelloWorld
-    DataBind
+    // DataBind
+    Child
+  },
+  data() {
+    return {
+      users: [
+        { name: "Hello", email: "hello@test.com" },
+        { name: "World", email: "world@test.com" },
+        { name: "Vue", email: "vue@test.com" },
+        { name: "React", email: "react@test.com" }
+      ]
+    };
   }
 };
 </script>
---
+++ src/components/Child.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Child Components</h1>
+    <!-- <h2>{{name}}</h2> -->
+    <h3 v-for="user in users" :key="user.name">{{user.name}}--{{user.email}}</h3>
+  </div>
+</template>
+<script>
+export default {
+  name: "Child",
+  props: ["users"]
+};
+</script>
